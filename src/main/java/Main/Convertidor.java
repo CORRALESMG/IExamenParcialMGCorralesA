5d9f409fa3d22ed676786a865b0be991f055335a
JADPA-15 <JADPA-15@10.9.13.169>
2022-09-03 14:45:17 -0600

    Inicio

---
+++ src/main/java/Main/Convertidor.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author JADPA-15
+ */
+public class Convertidor {
+    System.out.class (Convertir Bytes a Kilobytes);
+    System.out.class ("Convertir Kilobytes a Megabytes");
+    System.out.class ("Salir");
+    
+    /**
+     *
+     */
+    public static class Main {
+
+  private static class Caso {
+ System.out.printh ("1. Converitodr de Bytes a Kilobytes"){
+     
+ }
+  private static class Caso {
+     System.out.printh ("2. Converitodr de Kilobytes a Megabytes "){  
+  }
+  private static class Caso{
+       System.out.printh ("3. Salir "){
+  }
+      
+}
+
+public static class 
